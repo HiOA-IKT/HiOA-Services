70af66b25c0b8af456a265bb399fca6db1aa0d40
Karl August Hoivik <gussefant@gmail.com>
2015-06-29 22:45:24 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Services</title>
+		<script src="jquery.min.js"></script>
+		<script src="services.js"></script>
+		<link rel="stylesheet" type="text/css" href="services.css">
+		<meta http-equiv="refresh" content="20" >
+	</head>
+	<body>
+		<?php
+			error_reporting(E_ALL ^ E_NOTICE);
+			require ("./services.php");
+		?>
+	</body>
+</html>
---
+++ services.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	// Services, dropping down errors
+	$(".title").click(function(){
+		$(this).siblings(".dropdown").slideToggle("fast");
+	});
+	
+	// Service errors, dropping down urls
+	$(".error").click(function(){
+		$(this).siblings(".urls").slideToggle("fast");
+	});
+	$("div.title:has(div.err-tag)").css("background-color","#f4cabc");
+});
