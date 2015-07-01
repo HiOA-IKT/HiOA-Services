d06a7aa85dc7263b15b6305da59c39a90c49baf7
Karl August Høivik <Karl-August.Hoivik@hioa.no>
2015-07-01 12:21:11 +0200

    Removed superfluous error-ids

--- services.php
+++ services.php
@@ -23,9 +23,9 @@ function print_file($file){
 	}
 	fclose($myfile);
 }
-function print_error($msg, $id){
+function print_error($msg){
 	echo "<li>";
-	echo "	<div class=\"error\" id=\"" . $id . "\">";
+	echo "	<div class=\"error\">";
 	echo "		" . $msg;
 	echo "		<div class=\"status err-tag\">Error</div>";
 	echo "	</div>";
@@ -42,18 +42,18 @@ function first_err(){
 function print_login_services(){
 	global $log_dir;
 	$stat = "ok";
-	$login_services = array("USER_UUID not set"=>"user-uuid-not-set", "Not login page"=>"loginpage", "Not logged in"=>"not-logged-in", "Logout failed"=>"logout");
+	$login_services = array("USER_UUID not set", "Not login page", "Not logged in", "Logout failed");
 	echo "	<div id=\"login-service\" class=\"service\">";
 	echo "		<div class=\"title\">";
 	echo "			Login Services";
-	foreach ($login_services as $msg => $id){
+	foreach ($login_services as $msg){
 		$file = $log_dir . $msg;
 		if(file_exists($file)){
 			if($stat == "ok"){
 				$stat = "err";
 				first_err();
 			}
-			print_error($msg, $id);
+			print_error($msg);
 		}
 	}
 	if($stat == "ok"){
@@ -69,18 +69,18 @@ function print_login_services(){
 function print_course_services(){
 	global $log_dir;
 	$stat = "ok";
-	$course_services=array("No bachelor courses found"=>"no-bach", "No master courses found"=>"no-master", "No one year courses found"=>"no-oneyr", "Unfinished bachelor course"=>"unf-bach", "Unfinished master course"=>"unf-master", "Unfinished one year course"=>"unf-oneyr", "Test user has no studies"=>"no-study");
+	$course_services=array("No bachelor courses found", "No master courses found", "No one year courses found", "Unfinished bachelor course", "Unfinished master course", "Unfinished one year course", "Test user has no studies");
 	echo "	<div id=\"courses\" class=\"service\">";
 	echo "		<div class=\"title\">";
 	echo "			Courses";
-	foreach ($course_services as $msg => $id){
+	foreach ($course_services as $msg){
 		$file = $log_dir . $msg;
 		if(file_exists($file)){
 			if($stat == "ok"){
 				$stat = "err";
 				first_err();
 			}
-			print_error($msg, $id);
+			print_error($msg);
 		}
 	}
 	if($stat == "ok"){
@@ -96,18 +96,18 @@ function print_course_services(){
 function print_info_services(){
 	global $log_dir;
 	$stat = "ok";
-	$info_services = array("Error in NSD numbers"=>"nsd-nums", "hioa.mynetworkglobal.com is down"=>"mng-down", "Missing Google Maps"=>"gm-missing", "No connection to MNG"=>"mng-conn", "Search for christ failed"=>"chr-search", "Search for Høivik failed"=>"hoi-search", "Filtered search failed"=>"filt-search", "StudentWeb is down"=>"studweb-down", "Fronter is down"=>"fronter-down", "Outlook is down"=>"outlook-down", "Room reservation is down"=>"romres-down", "Inspera is down"=>"inspera-down");
+	$info_services = array("Error in NSD numbers", "hioa.mynetworkglobal.com is down", "Missing Google Maps", "No connection to MNG", "Search for christ failed", "Search for Høivik failed", "Filtered search failed", "StudentWeb is down", "Fronter is down", "Outlook is down", "Room reservation is down", "Inspera is down");
 	echo "	<div id=\"info\" class=\"service\">";
 	echo "		<div class=\"title\">";
 	echo "			Information";
-	foreach ($info_services as $msg => $id){
+	foreach ($info_services as $msg){
 		$file = $log_dir . $msg;
 		if(file_exists($file)){
 			if($stat == "ok"){
 				$stat = "err";
 				first_err();
 			}
-			print_error($msg, $id);
+			print_error($msg);
 		}
 	}
 	if($stat == "ok"){
@@ -123,18 +123,18 @@ function print_info_services(){
 function print_solr_services(){
 	global $log_dir;
 	$stat = "ok";
-	$solr_services = array("Error in solrdata publications"=>"solr-pub", "Solr connection does not exist"=>"solr-conn");
+	$solr_services = array("Error in solrdata publications", "Solr connection does not exist");
 	echo "	<div id=\"solr\" class=\"service\">";
 	echo "		<div class=\"title\">";
 	echo "			Solr";
-	foreach ($solr_services as $msg => $id){
+	foreach ($solr_services as $msg){
 		$file = $log_dir . $msg;
 		if(file_exists($file)){
 			if($stat == "ok"){
 				$stat = "err";
 				first_err();
 			}
-			print_error($msg, $id);
+			print_error($msg);
 		}
 	}
 	if($stat == "ok"){
@@ -150,18 +150,18 @@ function print_solr_services(){
 function print_profile_services(){
 	global $log_dir;
 	$stat = "ok";
-	$profile_services = array("Cristin data does not exist"=>"cristin", "Christ is unemployed"=>"christ-unemp");
+	$profile_services = array("Cristin data does not exist", "Christ is unemployed");
 	echo "	<div id=\"profiles\" class=\"service\">";
 	echo "		<div class=\"title\">";
 	echo "			User Profiles";
-	foreach ($profile_services as $msg => $id){
+	foreach ($profile_services as $msg){
 		$file = $log_dir . $msg;
 		if(file_exists($file)){
 			if($stat == "ok"){
 				$stat = "err";
 				first_err();
 			}
-			print_error($msg, $id);
+			print_error($msg);
 		}
 	}
 	if($stat == "ok"){
