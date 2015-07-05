3315c7a1319c0109aaada38d6019c621a16bb4da
Karl August Høivik <Karl-August.Hoivik@hioa.no>
2015-07-05 05:12:22 +0200

    Fixed duplicating tests and errors on server restart. Prettified things. Added alerts. Added client-requested testing.

--- HiOA Services.js
+++ HiOA Services.js
@@ -9,27 +9,41 @@ var exec = require('child_process').exec;
 //global.jQuery = $;
 var unique = require('array-unique');
 //require('bootstrap');
-/*var vantage = require('vantage')();
-vantage
-	.command("foo")
-	.description("Outputs 'bar'.")
+var vantage = require('vantage')();
+var logdir = "/home/***REMOVED***/jmetertesting/logs";
+var testdir = "/home/***REMOVED***/jmetertesting";
+/*vantage
+	.command("cleanlogs")
+	.description("Clears all error logs on the server.")
 	.action(function(args,cb){
-		console.log("bar");
-		cb();
+		this.prompt({
+			type: "confirm",
+			name: "continue",
+			default: false,
+			message: "Are you sure you want to clear logs?",
+		}, function(result){
+			if(!result.continue){
+				console.log("Good choice.");
+				cb();
+			} else{
+				console.log("All clean. :)");
+				ls = exec('ls -la ' + logdir, function(error, stdout, stderr){
+					console.log(stdout);
+					cb();
+				});
+			}
+		});
 	});
 
 vantage
-	.delimiter('webapp~$')
+	.delimiter('HiOA Services~$')
 	.listen(1234)
 	.show();
 */
-var logdir = "/home/***REMOVED***/jmetertesting/logs";
-var testdir = "/home/***REMOVED***/jmetertesting/YML";
 var watcher = chokidar.watch(logdir, {
 	ignored: /[\/\\]\./, 
 	persistent: true
 });
-
 watcher.on('add',function(path){
 	fs.readFile(path, "utf8", function(err, data){
 		if(err) throw err;
@@ -37,7 +51,6 @@ watcher.on('add',function(path){
 		var file = path_ar.pop();
 		var cat = path_ar.pop();
 		extract_and_emit("err-add", cat, file, data, "all");
-		console.log("Error \""+file+"\"");
 	});
 })
 .on('change', function(path){
@@ -54,7 +67,6 @@ watcher.on('add',function(path){
 	var file = path_ar.pop();
 	var cat = path_ar.pop();
 	io.emit('err-rm', cat, file);
-	console.log("Error \""+file+"\" fixed! :D");
 })
 function extract_and_emit(ev, cat, file, data, target){
 	var data_ar = unique(data.split("\n"));
@@ -73,31 +85,49 @@ function extract_and_emit(ev, cat, file, data, target){
 app.use(express.static(__dirname));
 http.listen(process.env.PORT || 80);
 io.on("connection", function(socket){
-	fs.readdir(testdir, function(err, tests){
-		if(err) throw err;
-		tests.forEach(function(test){
-			var name = test.slice(0, -4);
-			socket.emit("new-test", testdir+"/"+test, name);
-			console.log("Sending test: "+testdir+"/"+test+" to connected user");
-		});
-	});
-	fs.readdir(logdir, function(err, dirs){
-		if(err) throw err;
-		dirs.forEach(function(dir){
-			fs.readdir(logdir+"/"+dir, function(err, files){
+	socket.on("init", function(magicword){
+		if(magicword=="Please"){
+			console.log("Initializing connected user.");
+			fs.readdir(testdir, function(err, tests){
 				if(err) throw err;
-				files.forEach(function(file){
-					fs.readFile(logdir+"/"+dir+"/"+file, "utf8", function(err, data){
+				tests.forEach(function(test){
+					var ext = test.substr(test.lastIndexOf('.')+1);
+					if(ext=="yml"){
+						var name = test.slice(0, -4);
+						socket.emit("new-test", testdir+"/"+test, name);
+					}
+				});
+			});
+			fs.readdir(logdir, function(err, dirs){
+				if(err) throw err;
+				dirs.forEach(function(dir){
+					fs.readdir(logdir+"/"+dir, function(err, files){
 						if(err) throw err;
-						extract_and_emit("err-add", dir, file, data, socket);
+						files.forEach(function(file){
+							fs.readFile(logdir+"/"+dir+"/"+file, "utf8", function(err, data){
+								if(err) throw err;
+								extract_and_emit("err-add", dir, file, data, socket);
+							});
+						});
 					});
 				});
 			});
-		});
+		}
 	});
-	socket.on("test_run", function(test, username, password, random_pages, log_dir){
-		socket.emit("running_test", { data: "Running" });
+	socket.on("test_run", function(test, username, password, random_pages, conc, iter, fn){
+		socket.emit("test_started", test.split("/").pop());
 		console.log("Running test...");
-		console.log("bzt \""+test+"\" -o execution.scenario.variables.username=\""+username+"\" -o execution.scenario.variables.password=\""+password+"\" -o execution.scenario.variables.random_pages=\""+random_pages+"\" -o execution.scenario.variables.logdir=\""+log_dir+"\""); 
+		var command = "bzt \""+test+"\" -o modules.console.disable=true -o execution.scenario.variables.username=\""+username+"\" -o execution.scenario.variables.password=\""+password+"\" -o execution.scenario.variables.random_pages="+random_pages+" -o execution.scenario.variables.logdir=\""+logdir+"\" -o execution.concurrency="+conc+" -o execution.iterations="+iter;
+		console.log(command);
+		yml_test = exec(command, function(error, stdout, stderr){
+		       console.log('stdout: '+stdout);
+		       console.log('stderr: '+stderr);
+		       if(error !== null){
+			       socket.emit("test_failed", test.split("/").pop(), stderr);
+			       console.log('exec error: ' + error);
+		       } else{
+		       		socket.emit("test_complete", test.split("/").pop());
+		       }
+	       });
 	});
 });
--- services.js
+++ services.js
@@ -1,4 +1,7 @@
 var socket = io();
+// Get data from server
+socket.emit("init", "Please");
+// Add a new error
 socket.on('err-add', function(cat, msg, urls){
 	var error_element = "<li>";
 	error_element+="	<div class=\"error panel panel-danger\">";
@@ -17,15 +20,18 @@ socket.on('err-add', function(cat, msg, urls){
 	$("#"+cat).find(".label").text("Error");
 	setRules();
 });
+// Update an error
 socket.on('err-update', function(cat, msg, urls){
 	$("#"+msg.split(" ").join("-")+"-urls").html(urls);
 });
+// Remove an error
 socket.on('err-rm', function(cat, msg){
 	$("#"+msg.split(" ").join("-")+"-urls").parent().parent().remove();
 	$("ul").not(':has(li)').parents(".panel-body").siblings(".panel-heading").children(".panel-title").children(".label").attr('class', "label label-success");
 	$("ul").not(':has(li)').parents(".panel-body").siblings(".panel-heading").children(".panel-title").children(".label").text("OK");
 	$("#"+cat).not(":has(.label-danger)").attr("class", "panel panel-success");
 });
+// Send tests to clients
 socket.on("new-test", function(path, name){
 	var test_element="<li>";
 	test_element+="		<div id=\""+name.split(" ").join("-")+"\" class=\"panel panel-default\">";
@@ -38,19 +44,27 @@ socket.on("new-test", function(path, name){
 	test_element+="			<div class=\"options panel-body\">";
 	test_element+="				<div class=\"input-group\">";
 	test_element+="					<span data-toggle=\"tooltip\" title=\"Username\" class=\"input-group-addon\" id=\"basic-addon1\">@</span>";
-	test_element+="					<input class=\"form-control\" placeholder=\"Username\" aria-describedby=\"basic-addon1\" id=\""+name.split(" ").join("-")+"-username\" type=\"text\">";
+	test_element+="					<input class=\"form-control\" value=\"***REMOVED***\" aria-describedby=\"basic-addon1\" id=\""+name.split(" ").join("-")+"-username\" type=\"text\">";
 	test_element+="				</div>";
 	test_element+="				<div class=\"input-group\">";
 	test_element+="					<span data-toggle=\"tooltip\" title=\"Password\" class=\"input-group-addon\" id=\"basic-addon2\">*</span>";
-	test_element+="					<input class=\"form-control\" placeholder=\"Password\" aria-describedby=\"basic-addon2\" id=\""+name.split(" ").join("-")+"-password\" type=\"text\">";
+	test_element+="					<input class=\"form-control\" value=\"***REMOVED***\" aria-describedby=\"basic-addon2\" id=\""+name.split(" ").join("-")+"-password\" type=\"text\">";
 	test_element+="				</div>";
 	test_element+="				<div class=\"input-group\">";
 	test_element+="					<span data-toggle=\"tooltip\" title=\"Random Pages\" class=\"input-group-addon\" id=\"basic-addon3\">?</span>";
 	test_element+="					<input class=\"form-control\" value=\"10\" aria-describedby=\"basic-addon3\" id=\""+name.split(" ").join("-")+"-random_pages\" type=\"number\">";
 	test_element+="				</div>";
-	test_element+="				<div class=\"input-group\">";
+	/*test_element+="				<div class=\"input-group\">";
 	test_element+="					<span data-toggle=\"tooltip\" title=\"Log Directory\" class=\"input-group-addon\" id=\"basic-addon4\">/</span>";
 	test_element+="					<input class=\"form-control\" value=\"../logs\" aria-describedby=\"basic-addon3\" id=\""+name.split(" ").join("-")+"-logdir\" type=\"text\">";
+	test_element+="				</div>";*/
+	test_element+="				<div class=\"input-group\">";
+	test_element+="					<span data-toggle=\"tooltip\" title=\"Concurrency\" class=\"input-group-addon\" id=\"basic-addon4\">||</span>";
+	test_element+="					<input class=\"form-control\" value=\"1\" aria-describedby=\"basic-addon3\" id=\""+name.split(" ").join("-")+"-conc\" type=\"text\">";
+	test_element+="				</div>";
+	test_element+="				<div class=\"input-group\">";
+	test_element+="					<span data-toggle=\"tooltip\" title=\"Iterations\" class=\"input-group-addon\" id=\"basic-addon4\">O</span>";
+	test_element+="					<input class=\"form-control\" value=\"1\" aria-describedby=\"basic-addon3\" id=\""+name.split(" ").join("-")+"-iter\" type=\"text\">";
 	test_element+="				</div>";
 	test_element+="			</div>";
 	test_element+="		</div>";
@@ -58,12 +72,16 @@ socket.on("new-test", function(path, name){
 	$("#tests").append(test_element);
 	setRules();
 });
+// Set jQuery rules
 function setRules(){
 	$(".panel-heading").unbind("click");
 	$(".error").unbind("click");
 	// Services, dropping down errors
 	$(".panel-heading").click(function(){
 		$(this).siblings(".panel-body").slideToggle("fast");
+	});
+	$(".btn").click(function(e){
+		e.stopPropagation();
 	});
 	// Service errors, dropping down urls
 	/*$(".error").click(function(){
@@ -77,10 +95,30 @@ function setRules(){
 	$(".panel-title:has(span.label-danger)").parents(".panel").attr("class","panel panel-danger");
 	$(".panel-title").not(":has(span.label-danger)").not(":has(button)").parents(".panel").attr("class", "panel panel-success");
 }
+// Request a test run from server
 function run_test(name, path){
 	var user = document.getElementById(name.split(" ").join("-")+"-username").value;
 	var pass = document.getElementById(name.split(" ").join("-")+"-password").value;
 	var pages = document.getElementById(name.split(" ").join("-")+"-random_pages").value;
-	var logdir = document.getElementById(name.split(" ").join("-")+"-logdir").value;
-	socket.emit("test_run", path, user, pass, pages, logdir);
+	//var logdir = document.getElementById(name.split(" ").join("-")+"-logdir").value;
+	var conc = document.getElementById(name.split(" ").join("-")+"-conc").value;
+	var iter = document.getElementById(name.split(" ").join("-")+"-iter").value;
+	socket.emit("test_run", path, user, pass, pages, conc, iter);
+}
+// Alerts
+socket.on("test_complete", function(test){
+	insert_alert("success", "Success!", test+" completed successfully.");
+});
+socket.on("test_failed", function(test, error){
+	insert_alert("danger", "Oops!", test+" failed with error: "+error);
+});
+socket.on("test_started", function(test){
+	insert_alert("info", "Info", test+" started.");
+});
+function insert_alert(type, header, msg){
+	var alert_element="<div class=\"alert alert-block ";
+	alert_element+="alert-"+type+"\" id=\"test-unknown\" role=\"alert\">";
+	alert_element+="<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times</span></button>";
+	alert_element+="<strong class=\"alert-header\">"+header+"</strong><p>"+msg+"</p></div>";
+	$(".alert-container").append(alert_element);	
 }
